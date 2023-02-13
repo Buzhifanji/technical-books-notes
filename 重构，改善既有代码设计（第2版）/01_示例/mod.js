29e32f716181b1577d65af48c9c72a8e16192ff3
buzhifanji <buzhifanji@163.com>
2023-02-13 18:48:33 +0800

    优化 重构测试 后比对结果

--- 重构，改善既有代码设计（第2版）/01_示例/mod.js
+++ 重构，改善既有代码设计（第2版）/01_示例/mod.js
@@ -8,7 +8,19 @@ const plays = JSON.parse(playsJSON);
 const invoices = JSON.parse(invoicesJSON);
 
 const result = statement(invoices[0], plays);
-console.log("result: " + result);
-
 const refactorResult = refactorStatement(invoices[0], plays);
-console.log("refactorResult: " + refactorResult);
+
+const oldResult = result.split("\n");
+const newResult = refactorResult.split("\n");
+
+let flag = true;
+for (let i = 0; i < oldResult.length; i++) {
+  if (oldResult[i] !== newResult[i]) {
+    console.log("错误：" + oldResult[i] + " " + newResult[i]);
+    flag = false;
+  }
+}
+
+if (flag) {
+  console.log("重构测试通过");
+}
