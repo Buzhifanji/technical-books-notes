8f4e7dc45fde669635a716b7ff7c1e1523f50154
buzhifanji <buzhifanji@163.com>
2023-02-13 20:18:51 +0800

    将 formate 函数名修改为usd

--- 重构，改善既有代码设计（第2版）/01_示例/refactor.js
+++ 重构，改善既有代码设计（第2版）/01_示例/refactor.js
@@ -42,7 +42,7 @@ export function refactorStatement(invoice, plays) {
     return volumeCredits;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
@@ -58,12 +58,12 @@ export function refactorStatement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
