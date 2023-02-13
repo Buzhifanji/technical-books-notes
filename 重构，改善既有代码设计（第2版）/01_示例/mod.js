d5f521285967b6b99e7c107160759f43a22efd85
buzhifanji <buzhifanji@163.com>
2023-02-13 18:41:57 +0800

    重构示例 提炼函数

--- 重构，改善既有代码设计（第2版）/01_示例/mod.js
+++ 重构，改善既有代码设计（第2版）/01_示例/mod.js
@@ -1,4 +1,5 @@
 import { statement } from "./statement.js";
+import { refactorStatement } from "./refactor.js";
 
 const playsJSON = await Deno.readTextFile("./plays.json");
 const invoicesJSON = await Deno.readTextFile("./invoices.json");
@@ -7,4 +8,7 @@ const plays = JSON.parse(playsJSON);
 const invoices = JSON.parse(invoicesJSON);
 
 const result = statement(invoices[0], plays);
-console.log(result);
+console.log("result: " + result);
+
+const refactorResult = refactorStatement(invoices[0], plays);
+console.log("refactorResult: " + refactorResult);
