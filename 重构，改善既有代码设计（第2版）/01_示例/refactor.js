485a11396d31db1aaceb437bfb1b7e98cfe79e45
buzhifanji <buzhifanji@163.com>
2023-02-14 12:39:20 +0800

    将 createStatementData 分离到一个新的文件

--- 重构，改善既有代码设计（第2版）/01_示例/refactor.js
+++ 重构，改善既有代码设计（第2版）/01_示例/refactor.js
@@ -1,71 +1,7 @@
-/**
- * 这个函数代码组织不清晰
- */
+import { createStatementData } from "./create_statement_data.js";
+
 export function refactorStatement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
-  const statementData = {}; // 数据中转，让 renderPlainText 只操作通过 data 参数传进来的数据，这样所有计算代码都可以被挪到 statement 函数中
-  statementData.customer = invoice.customer;
-  // 生成副本，避免修改传递过来的数据
-  statementData.performances = invoice.performances.map(enrichPerformances);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-  return statementData;
-
-  function enrichPerformances(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-
-    return result;
-  }
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    // add volume credits
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === aPerformance.play.type)
-      volumeCredits += Math.floor(aPerformance.audience / 5);
-
-    return volumeCredits;
-  }
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
 }
 
 function renderPlainText(data) {
